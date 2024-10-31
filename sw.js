50b075f704f2550f13adc65015703f69c8fc4958
Bruno Oliveira <bruno.oliveira@smartfastpay.com>
2024-10-31 14:56:15 -0300

    configuração pwa

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'super-compras-v1';
+const urlsToCache = [
+  '/super-compras/',
+  '/super-compras/index.html',
+  '/super-compras/new.html',
+  '/super-compras/manifest.json',
+  '/super-compras/icons/icon-192x192.png',
+  '/super-compras/icons/icon-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/js/all.min.js'
+];
+
+// Instalação do service worker e cache dos recursos necessários
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(self.skipWaiting())
+  );
+});
+
+// Ativação do service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => 
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Interceptação das solicitações e recuperação do cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => 
+      response || fetch(event.request)
+    )
+  );
+});
